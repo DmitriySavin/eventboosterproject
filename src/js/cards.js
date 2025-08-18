7c916ff58d89ee4da627f56a29b7ce6f6dd757ea
Anton <141570541+Anonymusm@users.noreply.github.com>
2025-08-18 18:33:42 +0200

    ДІМА НЕ ПРИЙМАЙ, це для ментора перевірка

---
+++ src/js/cards.js
@@ -0,0 +1,131 @@
+import eurovisionImg from '../images/eurovision.png';
+import blackeyedImg from '../images/blackeyed.png';
+import lpImg from '../images/lp.png';
+import monatikImg from '../images/monatik.png';
+
+export const cards = [
+  {
+    title: 'Eurovision 2021 finals!',
+    date: '2021-05-13',
+    place: 'Palace of Ukraine',
+    image: eurovisionImg,
+  },
+  {
+    title: 'Atlas Weekend',
+    date: '2021-06-09',
+    place: 'VDNH',
+    image: blackeyedImg,
+  },
+  {
+    title: 'LP',
+    date: '2021-07-27',
+    place: 'Palace of Ukraine',
+    image: lpImg,
+  },
+  {
+    title: 'MONATIK',
+    date: '2021-07-17',
+    place: 'VDNH',
+    image: monatikImg,
+  },
+
+  {
+    title: 'MONATIK',
+    date: '2021-07-17',
+    place: 'VDNH',
+    image: monatikImg,
+  },
+  {
+    title: 'LP',
+    date: '2021-07-27',
+    place: 'Palace of Ukraine',
+    image: lpImg,
+  },
+  {
+    title: 'Atlas Weekend',
+    date: '2021-06-09',
+    place: 'VDNH',
+    image: blackeyedImg,
+  },
+  {
+    title: 'Eurovision 2021 finals!',
+    date: '2021-05-13',
+    place: 'Palace of Ukraine',
+    image: eurovisionImg,
+  },
+
+  {
+    title: 'Atlas Weekend',
+    date: '2021-06-09',
+    place: 'VDNH',
+    image: blackeyedImg,
+  },
+  {
+    title: 'Eurovision 2021 finals!',
+    date: '2021-05-13',
+    place: 'Palace of Ukraine',
+    image: eurovisionImg,
+  },
+  {
+    title: 'MONATIK',
+    date: '2021-07-17',
+    place: 'VDNH',
+    image: monatikImg,
+  },
+  {
+    title: 'LP',
+    date: '2021-07-27',
+    place: 'Palace of Ukraine',
+    image: lpImg,
+  },
+
+  {
+    title: 'Eurovision 2021 finals!',
+    date: '2021-05-13',
+    place: 'Palace of Ukraine',
+    image: eurovisionImg,
+  },
+  {
+    title: 'MONATIK',
+    date: '2021-07-17',
+    place: 'VDNH',
+    image: monatikImg,
+  },
+  {
+    title: 'LP',
+    date: '2021-07-27',
+    place: 'Palace of Ukraine',
+    image: lpImg,
+  },
+  {
+    title: 'Atlas Weekend',
+    date: '2021-06-09',
+    place: 'VDNH',
+    image: blackeyedImg,
+  },
+
+  {
+    title: 'LP',
+    date: '2021-07-27',
+    place: 'Palace of Ukraine',
+    image: lpImg,
+  },
+  {
+    title: 'Atlas Weekend',
+    date: '2021-06-09',
+    place: 'VDNH',
+    image: blackeyedImg,
+  },
+  {
+    title: 'MONATIK',
+    date: '2021-07-17',
+    place: 'VDNH',
+    image: monatikImg,
+  },
+  {
+    title: 'Eurovision 2021 finals!',
+    date: '2021-05-13',
+    place: 'Palace of Ukraine',
+    image: eurovisionImg,
+  },
+];
